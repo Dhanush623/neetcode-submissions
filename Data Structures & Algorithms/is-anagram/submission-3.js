e66f374ce78e4a79d02d4da06854798c1598de2d
Dhanush <40487848+Dhanush623@users.noreply.github.com>
2026-09-18 17:44:43 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.js
@@ -0,0 +1,13 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if (s.length != t.length) return false
+        const a = Array.from(s).sort()
+        const b = Array.from(t).sort()
+        return (JSON.stringify(a) === JSON.stringify(b))
+    }
+}
